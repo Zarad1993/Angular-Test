7979db5980ca64a3e1d1190dfd742887e7e5ac48
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-26 10:35:18 +0300

    Took out the controllers to a different folder

--- app.js
+++ app.js
@@ -1,48 +1,5 @@
-var ngTest = angular.module('ngTest', ['ui.router', 'ngAnimate']);
-
-ngTest.controller('TestController', function($scope, productService, $location){
-	$scope.addProduct = function(){
-		productService.addProduct($scope.newProduct)
-		$scope.newProduct = ''
-		$location.path('/home/2/Second')
-	}
-	$scope.firstSum = 0
-	$scope.secondSum = 0
-	$scope.thirdSum = 0
-	$scope.total = 0
-	$scope.update = function(){
-    $scope.total = 0
-    var array = [parseInt($scope.firstSum),parseInt($scope.secondSum),parseInt($scope.thirdSum)]
-     for(var i = 0; i < array.length; i++){
-       if(array[i].toString() === 'NaN'){
-        continue;
-       } else {
-        $scope.total += array[i]
-       }
-     }
-
-	};
-
-	$scope.newNumber = function(){
-    $scope.secondSum = ($scope.total - $scope.firstSum) / 2
-    $scope.thirdSum = ($scope.total - $scope.firstSum) / 2
-	}
-
-})
-ngTest.controller('SecondController', function($scope, productService){
-	console.log(productService.getProducts())
-	console.log('Got to the Second controller')
-	$scope.list = productService.getProducts()
-})
-ngTest.controller('ThirdController', function($scope){
-	$scope.sortType     = 'name'; // set the default sort type
-	$scope.sortReverse  = false;  // set the default sort order
-	$scope.searchCountry   = '';     // set the default search/filter term
-	$scope.longArray = data;
-})
-
+var ngTest = angular.module('ngTest', ['ui.router', 'ngAnimate','first','second','third']);
 ngTest.config(function($stateProvider, $urlRouterProvider) {
- 
     $stateProvider
         .state('home', {
             url: '/home/:page/:ctrl',
